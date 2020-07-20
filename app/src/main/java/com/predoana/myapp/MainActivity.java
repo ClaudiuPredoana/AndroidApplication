7ed269eeb9e25913eb5b89eb52a8a5fde8fd60b9
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-20 11:47:32 +0300

    Add android activity lifecycle

--- app/src/main/java/com/predoana/myapp/MainActivity.java
+++ app/src/main/java/com/predoana/myapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.predoana.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected  void onCreate (Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("myapp","onCreate");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("myapp","onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("myapp","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("myapp", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("myapp", "onStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("myapp", "onDestroy");
     }
 }
